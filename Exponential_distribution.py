fba0c15a76d0c123a1203bea47f703d93d2c331e
jithishj <66731388+jithishj@users.noreply.github.com>
2020-07-25 18:56:21 +0530

    Create Exponential_distribution.py

---
+++ Exponential_distribution.py
@@ -0,0 +1,18 @@
+#Exponential Distribution
+p=.5
+c=0
+l=[]
+for num in range(1000):
+    def bernoulli(p):
+        r=np.random.random()
+        if r<p:
+            return 1
+        else:
+            return 0
+             
+    while(bernoulli(p)==0):
+        c+=1
+    l.append(c)
+    c=0
+plt.hist(l)
+plt.show()
